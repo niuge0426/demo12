048bafe99356fa19bf7b70e8f315ac3ad225890f
Administrator <Administrator@PC-201811122201>
2018-11-22 21:29:21 +0800

    gsdgsDGsdg

--- src/main/java/com/example/demo12/sdg.java
+++ src/main/java/com/example/demo12/sdg.java
@@ -3,8 +3,6 @@ package com.example.demo12;
 public class sdg {
 	
 	public static void main(String[] args) {
-		System.out.println("");
-		System.out.println("test");
 		System.out.println("牛璐璐测试");
 		System.out.println("test");
 	}
